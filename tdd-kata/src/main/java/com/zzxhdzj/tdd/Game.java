961ade364952584b827c0284bb7b0da6a7a96531
ampm <hellooooy@gmail.com>
2013-10-22 01:23:23 +0800

    test01 failed#1

--- tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
+++ tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
@@ -8,4 +8,11 @@ package com.zzxhdzj.tdd;
  * To change this template use File | Settings | File Templates.
  */
 public class Game {
+    public void roll(int i) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public int score() {
+        return -1;  //To change body of created methods use File | Settings | File Templates.
+    }
 }
--- tdd-kata/src/test/java/com/zzxhdzj/tdd/AppTest.java
+++ tdd-kata/src/test/java/com/zzxhdzj/tdd/AppTest.java
@@ -2,6 +2,8 @@ package com.zzxhdzj.tdd;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Unit test for simple App.
  */
@@ -9,5 +11,9 @@ public class AppTest {
     @Test
     public void test_gutter_game() throws Exception{
         Game game = new Game();
+        for (int i=0;i<20;i++) {
+            game.roll(0);
+        }
+        assertEquals(0,game.score());
     }
 }
