f8239cd64b9f7e22bf631cfe37b79840254a8cf5
ampm <hellooooy@gmail.com>
2013-10-22 01:27:08 +0800

    test02 failed#1

--- tdd-kata/src/test/java/com/zzxhdzj/tdd/AppTest.java
+++ tdd-kata/src/test/java/com/zzxhdzj/tdd/AppTest.java
@@ -16,4 +16,12 @@ public class AppTest {
         }
         assertEquals(0,game.score());
     }
+    @Test
+    public void test_all_ones()throws Exception{
+        Game game = new Game();
+        for (int i = 0;i<20;i++){
+            game.roll(i);
+            assertEquals(20,game.score());
+        }
+    }
 }
