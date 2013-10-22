23559144df4c47fd48e601ebadefb167c9938f6c
ampm <hellooooy@gmail.com>
2013-10-22 14:12:05 +0800

    test04:o:ok,test_one_strike&rename frameIndex to rollIndex

--- tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
+++ tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
@@ -10,26 +10,30 @@ package com.zzxhdzj.tdd;
 public class Game {
     private int[] rolls = new int[21];
     private int currentRoll = 0;
+
     public void roll(int pins) {
         rolls[currentRoll++] = pins;
     }
 
     public int score() {
         int score = 0;
-        int frameIndex = 0;
-        for (int frame = 0;frame<10;frame++){
-            if(isSpare(frameIndex)){
-             score += 10+rolls[frameIndex+2];
-                frameIndex += 2;
-            }else{
-              score += rolls[frameIndex]+rolls[frameIndex+1];
-                frameIndex += 2;
+        int rollIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[rollIndex]==10) {
+                score += 10 + rolls[rollIndex + 1]+rolls[rollIndex + 2];
+                rollIndex++;
+            } else if (isSpare(rollIndex)) {
+                score += 10 + rolls[rollIndex + 2];
+                rollIndex += 2;
+            } else {
+                score += rolls[rollIndex] + rolls[rollIndex + 1];
+                rollIndex += 2;
             }
         }
         return score;
     }
 
-    private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex]+rolls[frameIndex+1]==10;
+    private boolean isSpare(int rollIndex) {
+        return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
     }
 }
