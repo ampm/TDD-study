5d52a131719e1e429107c4d8740a6b10cbcbbd50
ampm <hellooooy@gmail.com>
2013-10-22 11:13:48 +0800

    alias the i to frameIndex,it is a cursor like stuff,point out the current calc frame.

--- tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
+++ tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
@@ -16,14 +16,14 @@ public class Game {
 
     public int score() {
         int score = 0;
-        int i = 0; //bad frame
+        int frameIndex = 0; //bad
         for (int frame = 0;frame<10;frame++){
-            if(rolls[i]+rolls[i+1]==10){//spare
-             score += 10+rolls[i+2];
-                i += 2;
+            if(rolls[frameIndex]+rolls[frameIndex+1]==10){//spare
+             score += 10+rolls[frameIndex+2];
+                frameIndex += 2;
             }else{
-              score += rolls[i]+rolls[i+1];
-                i += 2;
+              score += rolls[frameIndex]+rolls[frameIndex+1];
+                frameIndex += 2;
             }
         }
         return score;
