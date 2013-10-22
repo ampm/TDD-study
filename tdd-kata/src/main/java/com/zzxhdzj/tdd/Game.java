38f795d5be1d0a2f806385c03edbbf1f10367a6a
ampm <hellooooy@gmail.com>
2013-10-22 10:42:40 +0800

    refactor the Game class by add a roll log array of  each roll pins  to the case when the player roll a spare ball

--- tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
+++ tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
@@ -9,12 +9,14 @@ package com.zzxhdzj.tdd;
  */
 public class Game {
     private int score = 0;
-
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
     public void roll(int pins) {
         score +=pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        return score;  //To change body of created methods use File | Settings | File Templates.
+        return score;
     }
 }
