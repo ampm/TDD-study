0ba553b76a19f4c3e9178bdfe0ffcfbd05e23c15
ampm <hellooooy@gmail.com>
2013-10-22 10:49:25 +0800

    refactor:make the roll  method just do the roll work

--- tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
+++ tdd-kata/src/main/java/com/zzxhdzj/tdd/Game.java
@@ -8,11 +8,9 @@ package com.zzxhdzj.tdd;
  * To change this template use File | Settings | File Templates.
  */
 public class Game {
-    private int score = 0;
     private int[] rolls = new int[21];
     private int currentRoll = 0;
     public void roll(int pins) {
-        score +=pins;
         rolls[currentRoll++] = pins;
     }
 
